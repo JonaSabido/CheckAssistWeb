1c3a8d4bfbdb84a9bee53ce0e493a09e6bbad056
JonaSabido <jona_sabido@hotmail.com>
2023-11-27 07:41:20 -0600

    dialog check nullable registered end

--- src/app/interfaces/checkuser.interface.ts
+++ src/app/interfaces/checkuser.interface.ts
@@ -4,9 +4,9 @@ import { Model } from "./model.interface";
 export interface CheckUser extends Model {
     id_meeting_user: number,
     start_date: string,
-    end_date: string,
+    end_date?: string,
     status: string,
     start_image: string,
-    end_image: string,
+    end_image?: string,
     meeting_user?: MeetingUser
 }
